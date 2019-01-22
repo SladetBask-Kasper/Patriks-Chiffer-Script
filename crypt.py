7803fef1247b1ee29a9ec00f124c621b53d0c663
Kasper <kasper4131@gmail.com>
2019-01-22 12:18:29 +0100

    Några små optimeringar.
    - Hjälp menyn har text nu.
    - En massa variablar från gammal kod urtagen ur decrypt()

--- crypt.py
+++ crypt.py
@@ -2,16 +2,15 @@ from os import system
 import collections
 from sys import argv
 
-def chunks(l, n): # l = array, n = chunksize?
+def chunks(l, n): # l = array(string), n = chunksize
 
-    x = 0
     chunk1 = []
 
     while True:
         try:
             if len(l) == 0:
                 break
-            chunk1.append(l[:n])#
+            chunk1.append(l[:n])
             l = l[n:]
         except IndexError:
             n -= 1
@@ -19,15 +18,13 @@ def chunks(l, n): # l = array, n = chunksize?
                 break
     return chunk1
 
-def crypt(iword = "_NONE_", ikey = "_NONE_"):
+def crypt(iword = "_NONE_", ikey = "_NONE_", kwargs = "_NONE_"):
     if iword == "_NONE_":
         iword = str(input("Enter Word: "))
     if ikey == "_NONE_":
         ikey = str(input("Enter Key: "))
     key = list(ikey)
     word = list(iword)
-    #word = ["A", "A", "V", "E", "L", "D", "T", "E", "T", "Ö", "D", "G"]
-    #key = ["2", "3", "1", "0", "2", "3", "1", "0"]
     conclution = ""
     y = 0
     x = len(word)
@@ -47,6 +44,10 @@ def crypt(iword = "_NONE_", ikey = "_NONE_"):
 
     print("conclution: " + str(conclution))
 
+    if kwargs.lower() == "hold":
+        input()
+        exit()
+
 def decrypt(iword = "_NONE_", ikey = "_NONE_", kwargs = "_NONE_"):
 
     if iword == "_NONE_":
@@ -54,25 +55,11 @@ def decrypt(iword = "_NONE_", ikey = "_NONE_", kwargs = "_NONE_"):
     if ikey == "_NONE_":
         ikey = str(input("Enter Key: "))
 
-    key = list(ikey)
-    word = list(iword)
-    #key = ["3", "5", "2", "1", "4", "0", "3", "5", "2", "1", "4", "0"]
-    #word = ["A", "A", "V", "E", "L", "D", "T", "E", "T", "Ö", "D", "G"]
-
-    converted_key = []
-
-    for i in key:
-        converted_key.append(int(i))
-
-    sorted_key = sorted(converted_key)
-    sorted_word = sorted(word)
-
     rev_key = str(ikey)[::-1]
     rev_word = str(iword)[::-1]
 
 
-    xx = len(rev_key)
-    crev_word = chunks(rev_word, xx)
+    crev_word = chunks(rev_word, len(rev_key))
     crev_word = crev_word[::-1]
     conclution = ""
 
@@ -90,6 +77,11 @@ def decrypt(iword = "_NONE_", ikey = "_NONE_", kwargs = "_NONE_"):
 
     if not kwargs.lower() == "quiet":
         print("conclution: " + str(conclution))
+
+    if kwargs.lower() == "hold":
+        input()
+        exit()
+
     return conclution
 
 def bruteforce(iword = "_NONE_", imin = "100", imax = "100000", scanmode = "_NONE_"):
@@ -125,7 +117,20 @@ def bruteforce(iword = "_NONE_", imin = "100", imax = "100000", scanmode = "_NON
 
 # hjälp meny
 def help_menu():
-    print("Help menu isn't working")
+    print(" --=^=--- Help menu --=^=--")
+    print("python_script.py (arg1) [(arg2) (arg3) (arg4) (arg5)]")
+    print("\t(arg1)--decrypt")
+    print("\t(arg1)--encrypt")
+    print("\t(arg1)--bruteforce")
+    print("\t(arg1)--search-for")
+    print("--------------------------------")
+    print("\t(arg2)msg")
+    print("\t(arg3)key (alt: min key)")
+    print("\t(arg4)maxkey")
+    print("\t(arg5)--scan-mode")
+
+    print("\n=============================\nFor more details, check out Github: \"https://github.com/SladetBask-Kasper/Patriks-Chiffer-Script\"\n=============================\n")
+
     exit()
 
 # Arg parser
@@ -137,9 +142,9 @@ if __name__ == "__main__":
         print("1: encrypt, 2: decrypt.")
         l = str(input(" > "))
         if l == "1":
-            crypt()
+            crypt(iword = "_NONE_", ikey = "_NONE_", kwargs = "hold")
         elif l == "2":
-            decrypt()
+            decrypt(iword = "_NONE_", ikey = "_NONE_", kwargs = "hold")
         else:
             print("Error- Invalid input value")
     else:
